71684a772bf65cafb47d3818307f7a5d2e19ee67
syifaulhz <syifaulhz@users.noreply.github.com>
2018-12-02 22:43:49 +0700

    tugasdw

---
+++ layout.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+<title>Layout Website</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div class="judul">
+        <img src="logo_rr.png" width="150px" height="75px">
+      </div>
+<div class="link"><a href="#">Home</a></div>
+<div class="colom"><br>
+    <a href="#"> Menu Link</a><br>
+    <a href="#"> Menu Link</a><br>
+    <a href="#"> Menu Link</a><br>  
+    <a href="#"> Menu Link</a><br>
+    <a href="#"> Menu Link</a><br>
+    <br>
+</div>
+<div class="content"><p><b>Page Heading</b><br>
+                     A three column layout with header,
+                 navigation bar and footer rows create the
+                 header, navigation bar and footer
+                 respecively and contain a single table
+                 cell each. All these tabe cells use colspan="3"
+                 attribute-value pair.<br><br>
+                 the third table row contains three table cells
+                 which create the menu column (left), the content
+                 column (middile) and the extra column (right).
+                 </p>
+                 <br>
+                 <div>
+                 Nama :Syifaul Huzni Nur'aizi<br>
+                 NIM : 5180411369<br></div>
+                 Kelas : F <br>
+                 Informastika S1<br>
+  </div>
+<div class="colom2"><br></div>
+ 
+ <div class="footer"> footer <div></div></div>
+
+
+
+
+</body></html>
